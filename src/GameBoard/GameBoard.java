28f7574b4e1b05355341e5baaef510f84f9f4868
Magdalena <63446437+Magdalena-Jekova@users.noreply.github.com>
2021-02-09 19:04:58 +0200

    Added mouseClicked event

--- src/GameBoard/GameBoard.java
+++ src/GameBoard/GameBoard.java
@@ -2,11 +2,14 @@ package GameBoard;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
-public class GameBoard extends JFrame {
+public class GameBoard extends JFrame implements MouseListener {
 
     public BoardTiles[][] boardTiles;
+    public BoardTiles clickedTile;
     private Random random;
     private int randomRow;
     private int randomCol;
@@ -19,6 +22,7 @@ public class GameBoard extends JFrame {
         this.setSize(800,800);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
+        this.addMouseListener(this);
     }
 
     /**
@@ -33,6 +37,70 @@ public class GameBoard extends JFrame {
                 tile.render(g);
             }
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int row = this.getBoardDimensionBasedOnCoordinates(e.getY());
+        int col = this.getBoardDimensionBasedOnCoordinates(e.getX());
+
+        if(row < 8 && col < 8) {
+            if (this.clickedTile != null) {
+                if (this.clickedTile.getColor().equals(Color.decode("#FFFF99"))) {
+                    generateTile(row, col);
+                    return;
+                }
+                if (this.clickedTile.getColor().equals(Color.decode("#EC7063"))) {
+                    this.clickedTile.setColor(Color.decode("#FFFF99"));
+                }
+                repaint();
+            }
+
+            if (this.hasBoardTile(row, col)) {
+                this.clickedTile = this.getBoardTile(row, col);
+                if (this.clickedTile.getColor().equals(Color.decode("#EC7063"))) {
+                    this.clickedTile.setColor(Color.decode("#FFFF99"));
+                    repaint();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    /**
+     * Метод, който генерира жълто или синьо поле върху дъската при втори клик с мишката върху първоначално избраното поле.
+     * @param row - редът, на който ще се генерира новото поле
+     * @param col - колоната, на която ще се генерира новото поле
+     */
+    private void generateTile(int row, int col){
+        int randomNumber = random.nextInt(10);
+        if(randomNumber == 0 || randomNumber == 1){
+            this.boardTiles[row][col] = new DangerTile(row, col);
+            repaint();
+        }else {
+            this.boardTiles[row][col] = new StartTile(row, col,"");
+            repaint();
+        }
+        this.clickedTile = null;
     }
 
     /**
@@ -122,5 +190,29 @@ public class GameBoard extends JFrame {
                 i--;
             }
         }
+    }
+
+    /**
+     * Метод, който връща поле от дъската, което се намира на съответния ред и колона.
+     * @param row - редът, на който се намира полето върху дъската
+     * @param col - колоната, на която се намира полето върху дъската
+     * @return object
+     */
+    private BoardTiles getBoardTile(int row, int col) {
+        return this.boardTiles[row][col];
+    }
+
+    /**
+     * Метод, който проверява дали на съответния ред и колона има поле върху дъската.
+     * @param row - редът, на който се проверява дали има поле
+     * @param col - колоната, на която се проверява дали има поле
+     * @return true, ако има поле и false, ако няма поле
+     */
+    private boolean hasBoardTile(int row, int col) {
+        return this.getBoardTile(row, col) != null;
+    }
+
+    private int getBoardDimensionBasedOnCoordinates(int coordinates) {
+        return coordinates / BoardTiles.BOARD_TILE_SIZE;
     }
 }
